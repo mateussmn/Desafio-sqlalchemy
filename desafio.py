f79cc870d357aa02ad7f741184d1f44795e98512
mateus <mateussilvamn18@gmail.com>
2026-07-15 19:36:41 -0300

    adicionando dependencias base

---
+++ desafio.py
@@ -0,0 +1,5 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+
+Base = declarative_base()
